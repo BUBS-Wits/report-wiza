9a6a28c455d5eeeb954e31464aca8345a65baa4d
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-04-03 19:09:10 +0200

    feat: send_request testing

--- tests/frontend/submit_request.js
+++ tests/frontend/submit_request.js
@@ -1,3 +1,12 @@
-const {assert_true, assert_false, test} = require("../test_framework.js")
+const {assert_equal, test} = require("../test_framework.js")
 const {ResidentRequest} = require("../../src/frontend/request.js")
+const {send_request} = require("../../src/frontend/submit_request.js")
 
+const ECHO_TEST = "https://httpbin.org/post"
+let TESTS = []
+
+test("send_request", async () => {
+	const tmp = new ResidentRequest("water", "water leakage.", "data:image/jpeg;base64,AD/CE...")
+	const response = await send_request(ECHO_TEST, tmp.to_json())
+	assert_equal((await response.json()).data, tmp.to_string())
+})
