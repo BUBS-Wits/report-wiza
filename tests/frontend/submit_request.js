f08bafc97c15e121f89ca621e0b9928aab15d9d2
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-04-03 19:12:15 +0200

    test: made the test name more explicit

--- tests/frontend/submit_request.js
+++ tests/frontend/submit_request.js
@@ -5,7 +5,7 @@ const {send_request} = require("../../src/frontend/submit_request.js")
 const ECHO_TEST = "https://httpbin.org/post"
 let TESTS = []
 
-test("send_request", async () => {
+test("service request send pass", async () => {
 	const tmp = new ResidentRequest("water", "water leakage.", "data:image/jpeg;base64,AD/CE...")
 	const response = await send_request(ECHO_TEST, tmp.to_json())
 	assert_equal((await response.json()).data, tmp.to_string())
