c7c1cc984175738eff50be245adc092fc07407d3
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-04-04 13:26:47 +0200

    chore: more consistency patching

--- src/report_web_vitals.js
+++ src/report_web_vitals.js
@@ -1,17 +1,17 @@
 const report_web_vitals = on_perf_entry => {
 	if (on_perf_entry && on_perf_entry instanceof Function) {
 		import('web-vitals').then(({
-			getCLS,
-			getFID,
-			getFCP,
-			getLCP,
-			getTTFB
+			get_cls,
+			get_fid,
+			get_fcp,
+			get_lcp,
+			get_ttfb
 		}) => {
-			getCLS(on_perf_entry)
-			getFID(on_perf_entry)
-			getFCP(on_perf_entry)
-			getLCP(on_perf_entry)
-			getTTFB(on_perf_entry)
+			get_cls(on_perf_entry)
+			get_fid(on_perf_entry)
+			get_fcp(on_perf_entry)
+			get_lcp(on_perf_entry)
+			get_ttfb(on_perf_entry)
 		})
 	}
 }
