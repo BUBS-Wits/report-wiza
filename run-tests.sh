61df65248924b674c6373fcca675f24eeef1be18
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-04-05 17:24:59 +0200

    chore: sigh...

--- run-tests.sh
+++ run-tests.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-if [[ ! -f "requirements.txt" ]]; then
-	exit
-fi
-
-set -f
-IFS=$'\n' reqs=( $(cat "requirements.txt" | sed -E "s/#.*//g") )
-set +f
-
-for req in "${reqs[@]}"; do
-	if [[ "$(npm list "$req" | grep -o "${req}")" != "${req}" ]]; then
-		./setup-requirements.sh
-	fi
-done
-
 node --trace-warnings ./tests/run_tests.js
 ret=$?
 if [[ -d ".git" ]]; then
--- tests/test_framework.js
+++ tests/test_framework.js
@@ -1,64 +1,64 @@
 const tests = []
 
 export function assert_equal(actual, expected) {
-    if (actual !== expected) {
-        throw new Error(`Equal assertion failed: ${actual} !== ${expected}`)
-    }
+	if (actual !== expected) {
+		throw new Error(`Equal assertion failed: ${actual} !== ${expected}`)
+	}
 }
 
 export function assert_not_equal(actual, expected) {
-    if (actual === expected) {
-        throw new Error(`Not equal assertion failed: ${actual} === ${expected}`)
-    }
+	if (actual === expected) {
+		throw new Error(`Not equal assertion failed: ${actual} === ${expected}`)
+	}
 }
 
 export function assert_less_than(actual, expected) {
-    if (actual >= expected) {
-        throw new Error(`Less than assertion failed: ${actual} >= ${expected}`)
-    }
+	if (actual >= expected) {
+		throw new Error(`Less than assertion failed: ${actual} >= ${expected}`)
+	}
 }
 
 export function assert_greater_than(actual, expected) {
-    if (actual <= expected) {
-        throw new Error(
-            `Greater than assertion failed: ${actual} <= ${expected}`,
-        )
-    }
+	if (actual <= expected) {
+		throw new Error(
+			`Greater than assertion failed: ${actual} <= ${expected}`
+		)
+	}
 }
 
 export function assert_true(value) {
-    if (!value) {
-        throw new Error(`True assertion failed: true !== ${Boolean(value)}`)
-    }
+	if (!value) {
+		throw new Error(`True assertion failed: true !== ${Boolean(value)}`)
+	}
 }
 
 export function assert_false(value) {
-    if (value) {
-        throw new Error(`True assertion failed: false !== ${Boolean(value)}`)
-    }
+	if (value) {
+		throw new Error(`True assertion failed: false !== ${Boolean(value)}`)
+	}
 }
 
 export function test(name, func) {
-    tests.push({ name, func })
+	tests.push({ name, func })
 }
 
 export async function run_tests() {
-    let passed = 0
-    for (const { name, func } of tests) {
-        try {
-            await func()
-            passed++
-            console.log(`\u2705 ${name}`)
-        } catch (err) {
-            console.log(`\u274C ${name}`)
-            console.error(`======`)
-            console.error(err)
-            console.error(`======`)
-        }
-    }
-    const percantage_pass = Math.round((passed / tests.length) * 10000, 2) / 100
-    console.log(`Passed Tests: ${passed}`)
-    console.log(`Failed Tests: ${tests.length - passed}`)
-    console.log(`Percentage Passed: ${percantage_pass}%`)
-    return percantage_pass == 100 ? 0 : 1
+	let passed = 0
+	for (const { name, func } of tests) {
+		try {
+			await func()
+			passed++
+			console.log(`\u2705 ${name}`)
+		} catch (err) {
+			console.log(`\u274C ${name}`)
+			console.error(`======`)
+			console.error(err)
+			console.error(`======`)
+		}
+	}
+	const percantage_pass = Math.round((passed / tests.length) * 10000, 2) / 100
+	console.log(`Passed Tests: ${passed}`)
+	console.log(`Failed Tests: ${tests.length - passed}`)
+	console.log(`Percentage Passed: ${percantage_pass}%`)
+	return percantage_pass == 100 ? 0 : 1
 }
