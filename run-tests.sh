56f40543df4c4223ad2cd771e37e5a49e10e2365
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-04-05 15:22:12 +0200

    chore: added an inconsequential log

--- run-tests.sh
+++ run-tests.sh
@@ -16,6 +16,9 @@ done
 
 node --trace-warnings ./tests/run_tests.js
 ret=$?
-echo -e "\e[0;32mReturn Code\e[0m: ${ret}"
+if [[ -d ".git" ]]; then
+	echo -e "\e[32mBranch\e[0m: $(git branch --show-current)"
+fi
+echo -e "\e[32mReturn Code\e[0m: ${ret}"
 
 exit ${ret}
