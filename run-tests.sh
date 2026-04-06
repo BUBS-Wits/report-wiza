7714ed000fb3f7ff69a959581e23480236c11fa9
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-04-06 09:08:36 +0200

    test: added coverage testing

--- run-tests.sh
+++ run-tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-node --trace-warnings ./tests/run_tests.js
-ret=$?
 if [[ -d ".git" ]]; then
 	echo -e "\e[32mBranch\e[0m: $(git branch --show-current)"
 fi
+node --trace-warnings ./tests/run_tests.js
+ret=$?
 echo -e "\e[32mReturn Code\e[0m: ${ret}"
 
 exit ${ret}
