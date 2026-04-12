7340a6bc566b6fee725e5f94edf95806e17cb2b6
sibusisombenenge <sibusisombenenge@gmail.com>
2026-04-12 20:25:03 +0200

    fix: move BrowserRouter to app.js, remove nested router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';  // ← add this
 import './app.css';
 import App from './app.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>      {/* ← wrap App with BrowserRouter */}
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
