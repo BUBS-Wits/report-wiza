884053c93b93d08d6a90df6cd4e9e9c1daf7e4b2
Germaine57 <jleahphil@gmail.com>
2026-04-12 04:14:41 +0200

    fix: move login route to /login

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReportWiza from './auth/report_wiza.js';
+import './app.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ReportWiza />} />
+        <Route path="/login" element={<ReportWiza />} />
         <Route path="/resident" element={<div>Resident Dashboard</div>} />
       </Routes>
     </BrowserRouter>
