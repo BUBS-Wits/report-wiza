b9b97ba37b39422afb64cc1601130df976c799a7
Germaine57 <jleahphil@gmail.com>
2026-04-12 17:32:50 +0200

    fix formatting

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/login_page/login.js';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/login_page/login.js'
 import LandingPage from './pages/landing_page/landing_page.js'
 import About from './pages/about_page/about.js'
 import Contact from './pages/contact_page/contact.js'
-import './app.css';
+import './app.css'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/resident" element={<div>Resident Dashboard</div>} />
-      </Routes>
-    </BrowserRouter>
-  );
+				<Route path="/login" element={<Login />} />
+				<Route
+					path="/resident"
+					element={<div>Resident Dashboard</div>}
+				/>
+			</Routes>
+		</BrowserRouter>
+	)
 }
-export default App;
+export default App
