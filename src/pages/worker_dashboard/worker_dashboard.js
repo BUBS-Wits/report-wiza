8c555a7945742aeabcebd82d210ae8d0089300ab
Germaine57 <jleahphil@gmail.com>
2026-05-10 01:27:28 +0200

    fix: prettier format

--- src/pages/worker_dashboard/worker_dashboard.js
+++ src/pages/worker_dashboard/worker_dashboard.js
@@ -23,7 +23,9 @@ import WorkerMessages from '../worker_messages/worker_messages.js'
 import './worker_dashboard.css'
 
 const parse_date = (val) => {
-	if (!val) {return '-'}
+	if (!val) {
+		return '-'
+	}
 	try {
 		const d = val.toDate ? val.toDate() : new Date(val)
 		return isNaN(d.getTime()) ? '-' : d.toISOString().split('T')[0]
