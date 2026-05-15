ba2be8297eeeb3dab1096b2f9cd22c238a389b0a
kiransoodyall03 <kiransoodyall03@gmail.com>
2026-05-15 22:07:42 +0200

    firebase config test

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebase_config = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'test-api-key',
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || 'test-auth-domain',
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || 'test-project-id',
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || 'test-storage-bucket',
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || 'test-sender-id',
+    appId: process.env.REACT_APP_FIREBASE_APP_ID || 'test-app-id',
 }
 
 const app = initializeApp(firebase_config)
