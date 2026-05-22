cdcf23f86b0bd40e30e2f0e03cf4ff885a89a3cd
kiransoodyall03 <kiransoodyall03@gmail.com>
2026-05-22 20:01:31 +0200

    Sneaky test fix

--- src/pages/resident_dashboard/resident_dashboard.js
+++ src/pages/resident_dashboard/resident_dashboard.js
@@ -446,6 +446,7 @@ export default function ResidentDashboard() {
 						<RequestDetail
 							req={selected_req}
 							resident={resident}
+							addMessage={addMessage}
 							on_back={() => set_selected_id(null)}
 							on_cancel={cancelRequest} // NEW prop
 						/>
@@ -528,7 +529,6 @@ function RequestDetail({ req, resident, on_back, on_cancel }) {
 	const [close_reason, set_close_reason] = useState(null)
 	const [close_reason_loading, set_close_reason_loading] = useState(false)
 	const [feedback_form, set_feedback_form] = useState(false)
-	const { addMessage } = useMessages()
 	const [messaging_enabled, set_messaging_enabled] = useState(
 		req.messaging_enabled !== false // initialise from request data so there's no flicker
 	)
